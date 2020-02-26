6e77db457b5aecd1916628de8ba0d13b5bd072e8
Oscar Brink <brinktjofzki@ludd.ltu.se>
2020-02-26 22:19:56 +0100

    Made ArtSlots into prefabs + non working slot scripts

---
+++ Assets/_Scripts/PaintingSlotScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Settings.;
+
+public class PaintingSlotScript : MonoBehaviour
+{
+    public SlotSettings Settings;
+    private bool a = false;
+
+    void Update()
+    {
+        if (!a)
+        {
+            a = true;
+            Debug.Log(Settings.SlotNumber);
+        }
+    }
+
+}
---
+++ Assets/_Scripts/SlotMasterScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotMasterScript : MonoBehaviour
+{
+
+    private int slots = 0;
+    //public RoomSettings roomSettings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string filename = "bil.png";
+        foreach (Transform child in transform)
+        {
+            Debug.Log(slots);
+            ArtMetaData artMeta = new ArtMetaData(
+                "title",
+                "artist",
+                filename,
+                ArtType.Painting
+            );
+            child.GetComponent<PaintingSlotScript>().Settings = new SlotSettings(
+                slots,
+                artMeta
+            );
+            ArtRegistry.GetArtRegistry().AddArt(artMeta);
+
+            //Sprite sprite = IMG2Sprite.instance.LoadNewSprite(filename);
+            //Debug.Log(sprite);
+
+            //child.GetComponent<SpriteRenderer>().sprite = sprite;
+            slots++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
