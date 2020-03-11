5868ab3c05d9e5aab909f87c9022af81062fbe2a
Christoffer Fink <chrfin-4@student.ltu.se>
2020-03-11 12:28:03 +0100

    Improved getting nested components
    - Made into extension methods
    - Added getting GameObjects (NestedObject)
    - Shortened name to just NestedComponent and NestedObject

--- Assets/_Scripts/UnityUtil.cs
+++ Assets/_Scripts/UnityUtil.cs
@@ -1,18 +1,35 @@
 using System;
 using UnityEngine;
 
-public class UnityUtil
+public static class UnityUtil
 {
 
-    public static T GetNestedComponent<T>(params string[] names)
-        => GetNestedComponent<T>(GameObject.Find(names[0]).transform, rest(names));
+    public static T NestedComponent<T>(params string[] names)
+        => NestedComponent<T>(GameObject.Find(names[0]).transform, rest(names));
 
-    public static T GetNestedComponent<T>(Transform root, params string[] names)
+    public static T NestedComponent<T>(this GameObject root, params string[] names)
+        => root.transform.NestedComponent<T>(names);
+
+    public static T NestedComponent<T>(this Transform root, params string[] names)
     {
         Transform tmp = root;
         foreach (string name in names)
             tmp = tmp.Find(name);
         return tmp.GetComponent<T>();
+    }
+
+    public static GameObject NestedObject(params string[] names)
+        => NestedObject(GameObject.Find(names[0]).transform, rest(names));
+
+    public static GameObject NestedObject(this GameObject root, params string[] names)
+        => NestedObject(root.transform, names);
+
+    public static GameObject NestedObject(this Transform root, params string[] names)
+    {
+        Transform tmp = root;
+        foreach (string name in names)
+            tmp = tmp.Find(name);
+        return tmp.gameObject;
     }
 
     private static T[] rest<T>(T[] arr)
